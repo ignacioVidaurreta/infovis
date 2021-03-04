3bafb7cbb31518e1025f52848b4321a8c403e9bf
nachito <igvidaurreta@gmail.com>
2021-03-04 12:51:37 -0300

    add code for the makeover monday challenge visualization

--- README.md
+++ README.md
@@ -3,3 +3,6 @@ Repository for the infovis project
 
 ### Structure
 The `docs` folder contains documents that contain theory or stuff I had to turn in.
+The `processing` folder contains `python` scripts used to process the data in order to get some visualizations.
+The `media` folder contains any static visualization (usually the output of any `processing` script)
+The `datasets` folder contains the datasets used for every visualization
---
+++ processing/seat_women.py
@@ -0,0 +1,36 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+
+COLS = 7
+ROWS = 4
+
+df = pd.read_csv("../datasets/seats_held_woman_eu.csv")
+
+# We need to do some preprocessing to remove NaN values
+df = df.dropna()
+df.reset_index(drop=True)
+
+fig, axes = plt.subplots(nrows=ROWS, ncols=COLS)
+fig.set_size_inches(30, 20)
+special_titles = ["EU 2020-", "EU 2013-2020"]
+
+total_countr = len(df)
+years = df.columns.values.tolist()[1:]
+
+current_row = 0;
+for i in range(total_countr):
+  frame = df.iloc[i, :]
+  title = frame.values[0]
+  if i <= 1:
+    title = special_titles[i]
+  women = frame.values[1:]
+  men = [ 1-x for x in women ]
+  d = {'year': years, 'women': women, 'men': men}
+  custom_df = pd.DataFrame(data=d)
+  if i != 0 and i % COLS == 0:
+    current_row+=1
+  custom_df.plot.area(x='year', title=title, ax=axes[current_row][i % COLS])
+
+
+fig.savefig("../media/seats_held_women_eu.png")
