8146f62bf15f71f03874de6c71875fa1f183ab1a
nachito <igvidaurreta@gmail.com>
2021-03-05 15:47:11 -0300

    show figures in descending order

--- processing/seat_women.py
+++ processing/seat_women.py
@@ -7,25 +7,31 @@ ROWS = 4
 
 df = pd.read_csv("../datasets/seats_held_woman_eu.csv")
 
+years = df.columns.values.tolist()[1:]
+
+# Replace the awful long names. Hardcoded and awful i'm sorry
+df["location"][0] = "EU 2020-"
+df["location"][1] = "EU 2013-2020"
+
 # We need to do some preprocessing to remove NaN values
 df = df.dropna()
+# We sort them for an easier view
+df = df.sort_values(by=years, ascending=False)
 df.reset_index(drop=True)
 
 fig, axes = plt.subplots(nrows=ROWS, ncols=COLS)
 fig.set_size_inches(30, 20)
-special_titles = ["EU 2020-", "EU 2013-2020"]
 
 total_countr = len(df)
-years = df.columns.values.tolist()[1:]
 
-current_row = 0;
+current_row = 0
 for i in range(total_countr):
   frame = df.iloc[i, :]
+
   title = frame.values[0]
-  if i <= 1:
-    title = special_titles[i]
   women = frame.values[1:]
   men = [ 1-x for x in women ]
+
   d = {'year': years, 'women': women, 'men': men}
   custom_df = pd.DataFrame(data=d)
   if i != 0 and i % COLS == 0:
