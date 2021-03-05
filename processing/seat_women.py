5d8796971bdd9696057b388627e132a5f8bec235
nachito <igvidaurreta@gmail.com>
2021-03-05 16:07:18 -0300

    remove redundant labels and fix title style

--- processing/seat_women.py
+++ processing/seat_women.py
@@ -21,6 +21,7 @@ df.reset_index(drop=True)
 
 fig, axes = plt.subplots(nrows=ROWS, ncols=COLS)
 fig.set_size_inches(30, 20)
+# plt.rcParams['axes.titlepad'] = -14
 
 total_countr = len(df)
 
@@ -36,7 +37,10 @@ for i in range(total_countr):
   custom_df = pd.DataFrame(data=d)
   if i != 0 and i % COLS == 0:
     current_row+=1
-  custom_df.plot.area(x='year', title=title, ax=axes[current_row][i % COLS])
-
+  axis = axes[current_row][i % COLS]
+  custom_df.plot.area(x='year', ax=axis)
+  axis.set_title(title, y=0.7, fontsize=20, color="white")
+  if i != 0:
+    axis.get_legend().remove() # Only keep first legend
 
 fig.savefig("../media/seats_held_women_eu.png")
