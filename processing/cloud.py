ea7c909cbd15da11aff485abe1affb9f6e424382
nachito <igvidaurreta@gmail.com>
2021-03-13 12:11:52 -0300

    update wordcloud and add wordcloud as jpg

--- processing/cloud.py
+++ processing/cloud.py
@@ -1,27 +1,31 @@
+import re
 import numpy as np
-import pandas as pd
-from os import path
 from PIL import Image
-from wordcloud import WordCloud, STOPWORDS, ImageColorGenerator
+from wordcloud import WordCloud, STOPWORDS
 import requests
-
 import matplotlib.pyplot as plt
+# Cool resource: https://towardsdatascience.com/simple-wordcloud-in-python-2ae54a9f58e5
+
+_RE_COMBINE_WHITESPACES = re.compile(r"\s+")
+
+def normalize_word(line):
+    return _RE_COMBINE_WHITESPACES.sub(" ", line.replace("-", " ")).strip().upper()
 
 def update_dict(current_dict, words):
     for word in words:
-        normalized = word.upper()
-        if normalized in current_dict:
-            current_dict[normalized] = current_dict[normalized] + 1
-        else:
-            current_dict[normalized] = 1
+        normalized = normalize_word(word)
+        if normalized != '':
+            if normalized in current_dict:
+                current_dict[normalized] = current_dict[normalized] + 1
+            else:
+                current_dict[normalized] = 1
 
     return current_dict
 
 
-
 with open("links.txt", "r") as f:
     links = f.readlines()
-    links = [ l.rstrip() for l in links] # Remove trailing newline
+    links = [l.rstrip() for l in links] # Remove trailing newline
 
 word_dict = {}
 for link in links:
@@ -29,8 +33,13 @@ for link in links:
     words = data.split("\n") # Get words
     word_dict = update_dict(word_dict, words)
 
-# print(word_dict)
-wordcloud = WordCloud(background_color=None, mode='RGBA', width=1000, height=1000, max_words=30, relative_scaling=0.5, normalize_plurals=True).generate_from_frequencies(word_dict)
-plt.imshow(wordcloud, interpolation='bilinear')
-plt.axis("off")
-plt.show()
+mask = np.array(Image.open('../media/comment_mask.png'))
+print(dict(sorted(word_dict.items(), key=lambda item: item[1], reverse=True)))
+wc = WordCloud(width=4000, height=3000, random_state=1, max_words=70,
+                      background_color='black', colormap='Pastel1', collocations=False,
+                      stopwords=STOPWORDS, normalize_plurals=True, mask=mask
+                      ).generate_from_frequencies(word_dict)
+# plt.axis("off")
+# plt.figure(figsize=(50, 40))
+# # plt.imshow(wordcloud, interpolation='bilinear')
+wc.to_file("../media/word_cloud.jpg")
